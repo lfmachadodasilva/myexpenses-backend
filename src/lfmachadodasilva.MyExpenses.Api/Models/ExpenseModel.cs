43e03c4b939b39ae813ef678e9afd103dd685cac
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2019-08-12 20:38:04 +0100

    created models #7

---
+++ src/lfmachadodasilva.MyExpenses.Api/Models/ExpenseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lfmachadodasilva.MyExpenses.Api.Models.Dtos
+{
+    [Table("Expense")]
+    public class ExpenseModel : ModelBase
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Value { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public ExpenseType Type { get; set; }
+
+        #region Relations
+
+        public long LabelId { get; set; }
+
+        [Required]
+        public long GroupId { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/lfmachadodasilva.MyExpenses.Api/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lfmachadodasilva.MyExpenses.Api.Models
+{
+    [Table("User")]
+    public class UserModel : ModelBase
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<UserGroupModel> UserGroups { get; set; }
+
+        public UserModel()
+        {
+            UserGroups = new List<UserGroupModel>();
+        }
+    }
+}
